867339042a5c6cb6b1d4410f40586919aa52d224
Stuff-Stuffs <stevenziehlke@gmail.com>
2022-05-14 13:58:08 -0500

    Basic Test Mod skeleton

---
+++ src/testmod/java/io/github/artificial_intellicrafters/merlin_ai_test/common/MerlinAITest.java
@@ -0,0 +1,12 @@
+package io.github.artificial_intellicrafters.merlin_ai_test.common;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class MerlinAITest implements ModInitializer {
+	public static final String MOD_ID = "merlin_ai_test";
+
+	@Override
+	public void onInitialize(final ModContainer mod) {
+	}
+}
