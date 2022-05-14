8a042592bc594d2f428fc08efa0c9f79ec5e09dc
Stuff-Stuffs <stevenziehlke@gmail.com>
2022-05-13 20:51:00 -0500

    Moved package to io.github.artificial_intellicrafters.merlin_ai

---
+++ src/main/java/io/github/artificial_intellicrafters/merlin_ai/client/MerlinAIClient.java
@@ -0,0 +1,10 @@
+package io.github.artificial_intellicrafters.merlin_ai.client;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class MerlinAIClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient(final ModContainer mod) {
+	}
+}
---
+++ src/main/java/io/github/artificial_intellicrafters/merlin_ai/common/MerlinAI.java
@@ -0,0 +1,20 @@
+package io.github.artificial_intellicrafters.merlin_ai.common;
+
+import net.minecraft.util.Identifier;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MerlinAI implements ModInitializer {
+	public static final String MOD_ID = "merlin_ai";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize(final ModContainer mod) {
+	}
+
+	public static Identifier createId(final String path) {
+		return new Identifier(MOD_ID, path);
+	}
+}
